ad04b57badd928fff18639aa5250e14dbc49db48
Derek Wan <d.wan@icloud.com>
2022-04-14 07:41:05 +0800

    Fix lints

--- src/bookr/settings.py
+++ src/bookr/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-icy6)4pqc17r9(9@2r#nb2n3(*cgcra7n5@zw(jgqssr(^n1=*"
-)
+SECRET_KEY = "django-insecure-icy6)4pqc17r9(9@2r#nb2n3(*cgcra7n5@zw(jgqssr(^n1=*"  # noqa: E501, S105
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,7 +87,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa: E501
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
