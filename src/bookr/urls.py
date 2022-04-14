8ae38430319c6845bf3ac29260c627f07a8103ed
Derek Wan <d.wan@icloud.com>
2022-04-14 20:45:11 +0800

    Define our own admin site

--- src/bookr/urls.py
+++ src/bookr/urls.py
@@ -1,6 +1,7 @@
-from django.contrib.admin import site
 from django.urls import include
 from django.urls import path
+
+from reviews.admin import site
 
 
 urlpatterns = [path("admin/", site.urls), path("", include("reviews.urls"))]
--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,1 +1,21 @@
-# Register your models here.
+from django.contrib.admin import AdminSite
+
+from reviews.models import Book
+from reviews.models import BookContributor
+from reviews.models import Contributor
+from reviews.models import Publisher
+from reviews.models import Review
+
+
+class BookrAdminSite(AdminSite):
+    title_header = "Bookr Admin"
+    site_header = "Bookr administration"
+    index_title = "Bookr site admin"
+
+
+site = BookrAdminSite(name="bookr")
+site.register(Publisher)
+site.register(Contributor)
+site.register(Book)
+site.register(BookContributor)
+site.register(Review)
