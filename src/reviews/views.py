c93d86d7bb08433757d9dae69a96b7b95b532894
Derek Wan <d.wan@icloud.com>
2022-04-14 08:07:04 +0800

    Move to 0.0.3

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from beartype import beartype
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+
+@beartype
+def index(_: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello, world!")
