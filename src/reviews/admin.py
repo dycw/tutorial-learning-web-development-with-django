27b99a6b013e1481ac168bca9b6a504f1df64820
Derek Wan <d.wan@icloud.com>
2022-04-14 21:57:45 +0800

    Finish chapter 4

--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,4 +1,7 @@
-from django.contrib.admin import ModelAdmin
+from typing import TYPE_CHECKING
+from typing import Any
+
+from django.contrib.admin import ModelAdmin as _ModelAdmin
 from django.contrib.admin import site
 
 from reviews.models import Book
@@ -8,26 +11,32 @@ from reviews.models import Publisher
 from reviews.models import Review
 
 
-class BookAdmin(ModelAdmin):  # type: ignore
+ModelAdmin = _ModelAdmin[Any] if TYPE_CHECKING else _ModelAdmin
+
+
+class BookAdmin(ModelAdmin):
     date_hierarchy = "publication_date"
     list_display = ("title", "isbn")
+    # or: >>> list_display = ("title", "isbn13")
     list_filter = ("publisher", "publication_date")
 
 
-# def initialled_name(obj):
-#     """ obj.first_names='Jerome David', obj.last_names='Salinger'
-#         => 'Salinger, JD' """
-#     initials = ''.join([name[0] for name in obj.first_names.split(' ')])
-#     return "{}, {}".format(obj.last_names, initials)
+def initialled_name(obj: Contributor, /) -> str:
+    """obj.first_names='Jerome David', obj.last_names='Salinger'
+    => 'Salinger, JD'"""
+    initials = "".join([name[0] for name in obj.first_names.split(" ")])
+    return f"{obj.last_names}, {initials}"
 
-# class ContributorAdmin(admin.ModelAdmin):
-#     list_display = ('last_names', 'first_names')
-#     list_filter = ('last_names',)
-#     search_fields = ('last_names__startswith', 'first_names')
+
+class ContributorAdmin(ModelAdmin):
+    list_display = ("last_names", "first_names")
+    # or: >>> list_display = (initialled_name,)
+    list_filter = ("last_names",)
+    search_fields = ("last_names__startswith", "first_names")
 
 
 site.register(Publisher)
-site.register(Contributor)
+site.register(Contributor, ContributorAdmin)
 site.register(Book, BookAdmin)
 site.register(BookContributor)
 site.register(Review)
