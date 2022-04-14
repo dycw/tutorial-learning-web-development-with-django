9de2ea1d00e8fad394630ee4cddd5cc104728ef1
Derek Wan <d.wan@icloud.com>
2022-04-14 15:49:08 +0800

    Add 0001

--- src/reviews/models.py
+++ src/reviews/models.py
@@ -1,1 +1,12 @@
-# Create your models here.
+from django.db.models import CharField
+from django.db.models import EmailField
+from django.db.models import Model
+from django.db.models import URLField
+
+
+class Publisher(Model):
+    """A company that publishes books."""
+
+    name = CharField(max_length=50, help_text="The name of the Publisher.")
+    website = URLField(help_text="The Publisher's website.")
+    email = EmailField(help_text="The Publisher's email address.")
