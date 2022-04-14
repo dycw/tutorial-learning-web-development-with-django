0cd4edb3dd7b8bc95b1c8f4ba6ae72ec4bf8658e
Derek Wan <d.wan@icloud.com>
2022-04-15 06:23:18 +0800

    Add static

--- src/business_site/business_site/settings.py
+++ src/business_site/business_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "business_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
