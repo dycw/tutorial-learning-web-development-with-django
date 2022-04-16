f5e74be86fe837efd414b31ae7bd5c8a9f45637a
Derek Wan <d.wan@icloud.com>
2022-04-16 17:24:02 +0800

    Finish chapter 10

--- src/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin import AdminSite
-
-
-class BookrAdminSite(AdminSite):
-    title_header = "Bookr Admin"
-    site_header = "Bookr administration"
-    index_title = "Bookr site admin"
---
+++ src/bookr_admin/admin.py
@@ -0,0 +1,25 @@
+from itertools import chain
+from typing import Any
+
+from django.contrib.admin import AdminSite
+from django.template.response import TemplateResponse
+from django.urls import URLResolver
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+
+class BookrAdmin(AdminSite):
+    site_header = "Bookr Administration"
+
+    def profile_view(self, request: Any) -> TemplateResponse:
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(request, "admin/admin_profile.html", context)
+
+    def get_urls(self) -> list[URLResolver | URLPattern]:  # type: ignore
+        return list(
+            chain(
+                super().get_urls(),
+                [path("admin_profile", self.admin_view(self.profile_view))],
+            )
+        )
