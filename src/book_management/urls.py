157c16d1fcba037c3a1a309e71778f899b83eee7
Derek Wan <d.wan@icloud.com>
2022-04-16 20:37:58 +0800

    Add

--- src/book_management/urls.py
+++ src/book_management/urls.py
@@ -1,12 +1,34 @@
 from django.urls import path
 
+from book_management.views import BookCreateView
+from book_management.views import BookDeleteView
+from book_management.views import BookRecordDetailView
 from book_management.views import BookRecordFormView
+from book_management.views import BookUpdateView
 from book_management.views import FormSuccessView
 
 
 urlpatterns = [
     path(
-        "new_book_record", BookRecordFormView.as_view(), name="book_record_form"
+        "new_book_record/",
+        BookRecordFormView.as_view(),
+        name="book_record_form",
     ),
-    path("entry_success", FormSuccessView.as_view(), name="form_success"),
+    path("entry_success/", FormSuccessView.as_view(), name="form_success"),
+    path("book_record_create/", BookCreateView.as_view(), name="book_create"),
+    path(
+        "book_record_update/<int:pk>/",
+        BookUpdateView.as_view(),
+        name="book_update",
+    ),
+    path(
+        "book_record_delete/<int:pk>/",
+        BookDeleteView.as_view(),
+        name="book_delete",
+    ),
+    path(
+        "book_record_detail/<int:pk>/",
+        BookRecordDetailView.as_view(),
+        name="book_detail",
+    ),
 ]
--- src/book_management/views.py
+++ src/book_management/views.py
@@ -6,7 +6,10 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import DetailView
 from django.views.generic import FormView
+from django.views.generic import UpdateView
 
 from book_management.forms import BookForm
 from book_management.models import Book
@@ -36,3 +39,23 @@ class BookCreateView(
     fields = ["name", "author"]
     template_name = "book_form.html"
     success_url = "/book_management/entry_success"
+
+
+class BookUpdateView(
+    UpdateView[Book, BookForm] if TYPE_CHECKING else UpdateView
+):
+    model = Book
+    fields = ["name", "author"]
+    template_name = "book_form.html"
+    success_url = "/book_management/entry_success"
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = "book_delete_form.html"
+    success_url = "/book_management/delete_success"
+
+
+class BookRecordDetailView(DetailView[Book] if TYPE_CHECKING else DetailView):
+    model = Book
+    template_name = "book_detail.html"
