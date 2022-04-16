ac5ab9b8b70a4edc53d7df264b93dca06df83087
Derek Wan <d.wan@icloud.com>
2022-04-16 17:24:33 +0800

    Finish chapter 10 (#15)

--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 from typing import Any
 
-from django.contrib.admin import ModelAdmin as _ModelAdmin
+from django.contrib.admin import ModelAdmin
 from django.contrib.admin import site
 
 from reviews.models import Book
@@ -11,14 +11,15 @@ from reviews.models import Publisher
 from reviews.models import Review
 
 
-ModelAdmin = _ModelAdmin[Any] if TYPE_CHECKING else _ModelAdmin
-
-
-class BookAdmin(ModelAdmin):
+class BookAdmin(ModelAdmin[Book] if TYPE_CHECKING else ModelAdmin):
+    model = Book
     date_hierarchy = "publication_date"
-    list_display = ("title", "isbn")
+    list_display = ("title", "isbn", "get_publisher", "publication_date")
     # or: >>> list_display = ("title", "isbn13")
     list_filter = ("publisher", "publication_date")
+
+    def get_publisher(self, obj: Any) -> str:
+        return obj.publisher.name
 
 
 def initialled_name(obj: Contributor, /) -> str:
@@ -28,7 +29,9 @@ def initialled_name(obj: Contributor, /) -> str:
     return f"{obj.last_names}, {initials}"
 
 
-class ContributorAdmin(ModelAdmin):
+class ContributorAdmin(
+    ModelAdmin[Contributor] if TYPE_CHECKING else ModelAdmin
+):
     list_display = ("last_names", "first_names")
     # or: >>> list_display = (initialled_name,)
     list_filter = ("last_names",)
