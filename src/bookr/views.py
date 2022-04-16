0a03baf955692ebbca0f107f49d2b23697767f9f
Derek Wan <d.wan@icloud.com>
2022-04-16 23:35:56 +0800

    fix

--- src/bookr/views.py
+++ src/bookr/views.py
@@ -1,7 +1,46 @@
+from typing import Any
+from typing import cast
+
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
+from plotly.graph_objects import Figure
+from plotly.graph_objects import Scatter
+from plotly.offline import plot
+
+from reviews.utils import get_books_read_by_month
 
 
+@login_required
 def profile(request: HttpRequest) -> HttpResponse:
-    return render(request, "profile.html")
+    user = cast(Any, request.user)
+    permissions = user.get_all_permissions()
+    # Get the books read in different months this year
+    books_read_by_month = get_books_read_by_month(user.username)
+
+    # Initialize the Axis for graphs, X-Axis is months, Y-axis is books read
+    months = [i + 1 for i in range(12)]
+    books_read = [0 for _ in range(12)]
+
+    # Set the value for books read per month on Y-Axis
+    for num_books_read in books_read_by_month:
+        list_index = num_books_read["date_created__month"] - 1
+        books_read[list_index] = num_books_read["book_count"]
+
+    # Generate a scatter plot HTML
+    figure = Figure()
+    scatter = Scatter(x=months, y=books_read)
+    figure.add_trace(scatter)
+    figure.update_layout(xaxis_title="Month", yaxis_title="No. of books read")
+    plot_html = plot(figure, output_type="div")
+
+    return render(
+        request,
+        "profile.html",
+        {
+            "user": user,
+            "permissions": permissions,
+            "books_read_plot": plot_html,
+        },
+    )
--- src/reviews/utils.py
+++ src/reviews/utils.py
@@ -1,4 +1,9 @@
+import datetime as dt
+from typing import Any
+
 from beartype import beartype
+from django.db.models import Count
+from django.db.models import QuerySet
 
 
 @beartype
@@ -7,3 +12,18 @@ def average_rating(rating_list: list[int], /) -> int:
         return round(sum(rating_list) / len(rating_list))
     else:
         return 0
+
+
+@beartype
+def get_books_read_by_month(username: str, /) -> QuerySet[Any]:
+    from reviews.models import Review
+
+    current_year = dt.datetime.now().year
+    return (
+        Review.objects.filter(
+            creator__username__contains=username,
+            date_created__year=current_year,
+        )
+        .values("date_created__month")
+        .annotate(book_count=Count("book__title"))
+    )
