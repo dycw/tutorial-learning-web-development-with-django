c93720b9744c5173367d147b9fc4602a50bfa923
Derek Wan <d.wan@icloud.com>
2022-04-16 22:48:10 +0800

    operatoin

--- src/reviews/serializers.py
+++ src/reviews/serializers.py
@@ -1,10 +1,18 @@
-from rest_framework.fields import ReadOnlyField
+from typing import Any
+from typing import cast
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+from rest_framework.exceptions import NotAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.fields import SerializerMethodField
+from rest_framework.relations import StringRelatedField
 from rest_framework.serializers import ModelSerializer
 
 from reviews.models import Book
-from reviews.models import BookContributor
-from reviews.models import Contributor
 from reviews.models import Publisher
+from reviews.models import Review
+from reviews.utils import average_rating
 
 
 class PublisherSerializer(ModelSerializer):
@@ -13,32 +21,80 @@ class PublisherSerializer(ModelSerializer):
         fields = ["name", "website", "email"]
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class ReviewSerializer(ModelSerializer):
+    creator = UserSerializer(read_only=True)
+    book = StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = [
+            "pk",
+            "content",
+            "date_created",
+            "date_edited",
+            "rating",
+            "creator",
+            "book",
+            "book_id",
+        ]
+
+    def create(self, validated_data: Any) -> Any:
+        request = self.context["request"]
+        creator = request.user
+        if not creator.is_authenticated:
+            raise NotAuthenticated("Authentication required.")
+        book = Book.objects.get(pk=request.data["book_id"])
+        return Review.objects.create(
+            content=validated_data["content"],
+            book=book,
+            creator=creator,
+            rating=validated_data["rating"],
+        )
+
+    def update(self, instance: Any, validated_data: Any) -> Any:
+        request = self.context["request"]
+        creator = request.user
+        if not creator.is_authenticated or instance.creator_id != creator.pk:
+            raise PermissionDenied(
+                "Permission denied, you are not the creator of this review"
+            )
+        instance.content = validated_data["content"]
+        instance.rating = validated_data["rating"]
+        instance.date_edited = timezone.now()
+        instance.save()
+        return instance
+
+
 class BookSerializer(ModelSerializer):
     publisher = PublisherSerializer()
+    rating = SerializerMethodField("book_rating")
+    reviews = SerializerMethodField("book_reviews")
+
+    def book_rating(self, book: Book) -> int | None:
+        if reviews := cast(Any, book).review_set.all():
+            return average_rating([review.rating for review in reviews])
+        else:
+            return None
+
+    def book_reviews(self, book: Book) -> Any:
+        if reviews := cast(Any, book).review_set.all():
+            return ReviewSerializer(reviews, many=True).data
+        else:
+            return None
 
     class Meta:
         model = Book
-        fields = ["title", "publication_date", "isbn", "publisher"]
-
-
-class ContributionSerializer(ModelSerializer):
-    book = BookSerializer()
-
-    class Meta:
-        model = BookContributor
-        fields = ["book", "role"]
-
-
-class ContributorSerializer(ModelSerializer):
-    bookcontributor_set = ContributionSerializer(read_only=True, many=True)
-    number_contributions = ReadOnlyField()
-
-    class Meta:
-        model = Contributor
         fields = [
-            "first_names",
-            "last_names",
-            "email",
-            "bookcontributor_set",
-            "number_contributions",
+            "title",
+            "publication_date",
+            "isbn",
+            "publisher",
+            "rating",
+            "reviews",
         ]
--- src/reviews/urls.py
+++ src/reviews/urls.py
@@ -1,7 +1,9 @@
+from django.urls import include
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from reviews.api_views import AllBooks
-from reviews.api_views import ContributorView
+from reviews.api_views import BookViewSet
+from reviews.api_views import ReviewViewSet
 from reviews.views import book_detail
 from reviews.views import book_list
 from reviews.views import book_media
@@ -11,10 +13,14 @@ from reviews.views import publisher_edit
 from reviews.views import review_edit
 
 
+router = DefaultRouter()
+router.register("books", BookViewSet)
+router.register("reviews", ReviewViewSet)
+
+
 urlpatterns = [
     path("", index),
-    path("api/all_books/", AllBooks.as_view(), name="all_books"),
-    path("api/contributors/", ContributorView.as_view(), name="contributors"),
+    path("api/", include((router.urls, "api"))),
     path("books/", book_list, name=book_list.__name__),
     path("books/<int:pk>/", book_detail, name=book_detail.__name__),
     path("books/<int:pk>/media/", book_media, name=book_media.__name__),
