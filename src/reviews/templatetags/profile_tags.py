a69a23b99ff5957741c5aaa70539d5b4157aca4e
Derek Wan <d.wan@icloud.com>
2022-04-16 20:47:36 +0800

    Finish chapter 11 (#16)
    * Fix up
    
    * Add
    
    * remove management

---
+++ src/reviews/templatetags/profile_tags.py
@@ -0,0 +1,13 @@
+from django.template import Library
+
+from reviews.models import Review
+
+
+register = Library()
+
+
+@register.inclusion_tag("book_list.html")
+def book_list(username: str) -> dict[str, list[str]]:
+    reviews = Review.objects.filter(creator__username__contains=username)
+    book_list = [review.book.title for review in reviews]
+    return {"books_read": book_list}
