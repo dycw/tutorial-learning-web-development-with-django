955c903a5bc3e4619e52bdabbe1248e6e23cbc3b
Derek Wan <d.wan@icloud.com>
2022-04-17 09:52:55 +0800

    fix

--- src/bookr/settings.py
+++ src/bookr/settings.py
@@ -34,9 +34,11 @@ class Dev(Configuration):
         "rest_framework",
         "rest_framework.authtoken",
         "reviews",
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
         "django.middleware.common.CommonMiddleware",
@@ -107,6 +109,8 @@ class Dev(Configuration):
     # Default primary key field type
     # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
     DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+    INTERNAL_IPS = ["127.0.0.1"]
 
 
 class Prod(Dev):
