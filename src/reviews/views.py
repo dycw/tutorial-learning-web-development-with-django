feeaf385610458ea24add635ada3a33000a4a74d
Derek Wan <d.wan@icloud.com>
2022-04-17 11:39:36 +0800

    fix

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -205,4 +205,4 @@ def book_media(request: HttpRequest, pk: int) -> HttpResponse:
 
 
 def react_example(request: HttpRequest) -> HttpResponse:
-    return render(request, "react-example.html")
+    return render(request, "react-example.html", {"name": "Ben", "target": 5})
